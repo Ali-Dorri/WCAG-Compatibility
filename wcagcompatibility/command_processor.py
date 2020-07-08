be8b04165f5f46972cc174fa893a8f94fbe2165e
AliDorri <dorri@unboundgamestudio.com>
2020-07-08 19:56:55 +0430

    implemented command processor

--- wcagcompatibility/command_processor.py
+++ wcagcompatibility/command_processor.py
@@ -1,53 +1,180 @@
+from selenium import webdriver
+from selenium.common.exceptions import InvalidArgumentException
+
+from wcagcompatibility.compatibility_tester import CompatibilityTester
 
 
 class CommandProcessor:
 
-    BROWSER_NOT_CREATED_ERROR = 'browser is not created yet'
+    BROWSER_NOT_CREATED_ERROR = 'browser is not created yet!'
 
     def __init__(self):
-        self.browser = None
+        self.compatibility_tester = None
 
     def process_command(self, command):
         """Call corresponding function according to command"""
+        command_args = command.split()
+        if command_args.__len__() == 0:
+            return None
+
+        print(command_args)
+        if command_args[0] == 'create_driver' or command_args[0] == 'create_webdriver':
+            if command_args.__len__() > 1:
+                self.create_webdriver(command_args[1])
+            else:
+                print('No input web driver was sent!')
+        elif command_args[0] == 'open_url':
+            if command_args.__len__() > 1:
+                self.open_url(command_args[1])
+            else:
+                print('No input url was sent!')
+        elif command_args[0] == 'test_all':
+            self.test_all()
+        elif command_args[0] == 'test_table_header':
+            self.test_table_header()
+        elif command_args[0] == 'test_html_language':
+            self.test_html_language()
+        elif command_args[0] == 'test_page_title':
+            self.test_page_title()
+        elif command_args[0] == 'test_html_deprecated':
+            self.test_html_deprecated()
+        elif command_args[0] == 'test_image_rect':
+            self.test_image_rect()
+        elif command_args[0] == 'test_table_description':
+            self.test_table_description()
+        elif command_args[0] == 'test_labeled_input_select_textarea':
+            self.test_labeled_input_select_textarea()
+        elif command_args[0] == 'test_identical_link_targets':
+            self.test_identical_link_targets()
+        else:
+            print('command is invalid')
 
     def create_webdriver(self, driver_name):
-        """Create corresponding webdriver and set it to browser"""
+        """Create corresponding webdriver and set it to compatibility tester"""
+        if driver_name == 'chrome':
+            self.set_browser(webdriver.Chrome())
+        elif driver_name == 'firefox':
+            self.set_browser(webdriver.Firefox())
+        elif driver_name == 'android':
+            self.set_browser(webdriver.Android())
+        elif driver_name == 'blackberry':
+            self.set_browser(webdriver.BlackBerry())
+        elif driver_name == 'edge':
+            self.set_browser(webdriver.Edge())
+        elif driver_name == 'ie':
+            self.set_browser(webdriver.Ie())
+        elif driver_name == 'opera':
+            self.set_browser(webdriver.Opera())
+        elif driver_name == 'phantomjs':
+            self.set_browser(webdriver.PhantomJS())
+        elif driver_name == 'remote':
+            self.set_browser(webdriver.Remote())
+        elif driver_name == 'safari':
+            self.set_browser(webdriver.Safari())
+        elif driver_name == 'webkitgk':
+            self.set_browser(webdriver.WebKitGTK())
+        else:
+            print('web driver name is invalid')
 
-    def set_url(self, url):
-        if self.browser is not None:
-            self.browser.open_url(url)
-            print('browser url set')
+    def set_browser(self, webdriver):
+        self.compatibility_tester = CompatibilityTester(webdriver)
+        print('web browser was opened')
+
+    def open_url(self, url):
+        if self.compatibility_tester is not None:
+            try:
+                self.compatibility_tester.open_url(url)
+                print('url was opened')
+            except InvalidArgumentException:
+                print('url is invalid')
         else:
             print(self.BROWSER_NOT_CREATED_ERROR)
 
-    def test_table_headers(self):
+    def test_all(self):
+        if self.compatibility_tester is not None:
+            self.test_table_header()
+            self.test_html_language()
+            self.test_page_title()
+            self.test_html_deprecated()
+            self.test_image_rect()
+            self.test_table_description()
+            self.test_labeled_input_select_textarea()
+            self.test_identical_link_targets()
+        else:
+            print(self.BROWSER_NOT_CREATED_ERROR)
+
+    def test_table_header(self):
         """Print if test_table_headers is verified or not"""
-        pass
+        if self.compatibility_tester is not None:
+            self.test_rule('table header', self.compatibility_tester.test_table_header)
+        else:
+            print(self.BROWSER_NOT_CREATED_ERROR)
 
     def test_html_language(self):
         """Print if test_html_language is verified or not"""
-        pass
+        if self.compatibility_tester is not None:
+            self.test_rule('html language', self.compatibility_tester.test_table_header)
+        else:
+            print(self.BROWSER_NOT_CREATED_ERROR)
 
-    def test_pages_title(self):
+    def test_page_title(self):
         """Print if test_pages_title is verified or not"""
-        pass
+        if self.compatibility_tester is not None:
+            self.test_rule('page title', self.compatibility_tester.test_page_title)
+        else:
+            print(self.BROWSER_NOT_CREATED_ERROR)
 
-    def test_html_deprecateds(self):
-        """Print if test_html_deprecateds is verified or not"""
-        pass
+    def test_html_deprecated(self):
+        """Print if test_html_deprecated attributes is verified or not"""
+        if self.compatibility_tester is not None:
+            self.test_rule('html deprecated', self.compatibility_tester.test_html_deprecated)
+        else:
+            print(self.BROWSER_NOT_CREATED_ERROR)
 
-    def test_image_width_height(self):
+    def test_image_rect(self):
         """Print if test_image_width_height is verified or not"""
-        pass
+        if self.compatibility_tester is not None:
+            self.test_rule('image rect area', self.compatibility_tester.test_image_rect)
+        else:
+            print(self.BROWSER_NOT_CREATED_ERROR)
 
     def test_table_description(self):
         """Print if test_table_description is verified or not"""
-        pass
+        if self.compatibility_tester is not None:
+            self.test_rule('table description', self.compatibility_tester.test_table_description)
+        else:
+            print(self.BROWSER_NOT_CREATED_ERROR)
 
     def test_labeled_input_select_textarea(self):
         """Print if test_labeled_input_select_textarea is verified or not"""
-        pass
+        if self.compatibility_tester is not None:
+            self.test_rule('labeled elements', self.compatibility_tester.test_labeled_input_select_textarea)
+        else:
+            print(self.BROWSER_NOT_CREATED_ERROR)
 
-    def test_identical_links_targets(self):
+    def test_identical_link_targets(self):
         """Print if test_identical_links_targets is verified or not"""
-        pass
+        if self.compatibility_tester is not None:
+            self.test_rule('identical links', self.compatibility_tester.test_identical_link_targets)
+        else:
+            print(self.BROWSER_NOT_CREATED_ERROR)
+
+    @staticmethod
+    def test_rule(rule_name, rule_test):
+        print('testing ' + rule_name + '...')
+        compliant = rule_test()
+        if compliant:
+            print(rule_name + ' rule is compliant')
+        else:
+            print(rule_name + ' rule is non compliant')
+
+
+if __name__ == "__main__":
+    processor = CommandProcessor()
+    while True:
+        try:
+            command = input()
+            processor.process_command(command)
+        except EOFError:
+            print('exited')
+            break
--- wcagcompatibility/compatibility_tester.py
+++ wcagcompatibility/compatibility_tester.py
@@ -3,13 +3,16 @@ from selenium import webdriver
 
 class CompatibilityTester:
 
-    def __init__(self, browser=webdriver.Firefox()):
-        self.browser = browser
+    def __init__(self, browser):
+        if browser is not None:
+            self.browser = browser
+        else:
+            self.browser = webdriver.Firefox()
 
     def open_url(self, url):
         self.browser.get(url)
 
-    def test_table_headers(self):
+    def test_table_header(self):
         """Return true if all tables has header, false otherwise"""
         pass
 
@@ -17,15 +20,16 @@ class CompatibilityTester:
         """Returns true if html has language attribute, false otherwise"""
         pass
 
-    def test_pages_title(self):
+    def test_page_title(self):
         """Returns true if all pages have head and title attributes, false otherwise"""
+        print('called test_page_title')
         pass
 
-    def test_html_deprecateds(self):
+    def test_html_deprecated(self):
         """Returns true if no deprecated html 5 attributes is used, false otherwise"""
         pass
 
-    def test_image_width_height(self):
+    def test_image_rect(self):
         """Returns true if all images has width and height attribute, false otherwise"""
         pass
 
@@ -37,6 +41,6 @@ class CompatibilityTester:
         """Returns true if all input, select and textarea elements have label, false otherwise"""
         pass
 
-    def test_identical_links_targets(self):
+    def test_identical_link_targets(self):
         """Returns true if all links with identical text have identical targets, false otherwise"""
         pass
